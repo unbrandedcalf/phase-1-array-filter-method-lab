39431818f1e4753fde35a01898b77b200efc3443
Maverick Vitto <mavvitto@yahoo.com>
2023-11-14 16:49:30 -0700

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string) {
+    const matchingDrivers = array.filter((name) => {
+      return name.toLowerCase() === string.toLowerCase();
+    });
+  
+    return matchingDrivers;
+  }  
+
+function fuzzyMatch(array, string) {
+    const matchingDrivers = array.filter((name) => {
+        return name.toLowerCase().startsWith(string.toLowerCase())
+    })
+    return matchingDrivers
+}
+
+function matchName(array, string) {
+    return array.filter( array => array.name === string)
+}
